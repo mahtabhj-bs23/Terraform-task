7b825a64146c25729074c68644e966386296687f
mohibul75 <bsse1004@iit.du.ac.bd>
2024-03-06 20:12:43 +0600

    Update ECS cluster name in outputs.tf to task-station-ecs-cluster.

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,5 +1,5 @@
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.piczangu.name
+  value = aws_ecs_cluster.task-station-ecs-cluster.name
 }
 output "ecs_service_sg_id" {
   value = aws_security_group.ecs-fargate.id
